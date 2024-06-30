0c4b99088e09cea22c89d0f0e693955a8e54bb6a
Haleem Akintayo <127564674+haleemakintayo@users.noreply.github.com>
2024-06-30 14:48:06 +0100

    Added Django components

---
+++ pages/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render,redirect
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def department(request):
+    return render(request, 'department.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+# def index(request):
+#     return render(request, 'index.html')
