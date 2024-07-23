066fe8d765f3188687af9a8d8125ec8d42bc61f8
Haleem Akintayo <127564674+haleemakintayo@users.noreply.github.com>
2024-07-23 02:25:34 +0100

    changed db to postgres

--- Quickaid/settings.py
+++ Quickaid/settings.py
@@ -105,10 +105,9 @@ GOOGLE_API_KEY = 'AIzaSyCWNkNcP_bWPCfcFL3fLMcRvQ0dj3Mb7pw'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgres://myuser:mypassword@localhost:5432/mydatabase'
+    )
 }
 
 
